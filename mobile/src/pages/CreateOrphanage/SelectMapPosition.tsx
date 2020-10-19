40abe55c6b59435eab25747f194d17e68b05ce43
Marco Echevestre <marcoaminotto@hotmail.com>
2020-10-19 09:56:23 +0200

    Implemente the CreateOrphanage pages

---
+++ mobile/src/pages/CreateOrphanage/SelectMapPosition.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { View, StyleSheet, Dimensions, Text } from 'react-native';
+
+import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
+import MapView, { Marker } from 'react-native-maps';
+
+import mapMarkerImg from '../../images/map-marker.png';
+
+export default function SelectMapPosition() {
+  const navigation = useNavigation();
+
+  function handleNextStep() {
+    navigation.navigate('OrphanageData');
+  }
+
+  return (
+    <View style={styles.container}>
+      <MapView 
+        initialRegion={{
+          latitude: 51.090114,
+          longitude: 17.016391,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008,
+        }}
+        style={styles.mapStyle}
+      >
+        <Marker 
+          icon={mapMarkerImg}
+          coordinate={{ latitude: 51.090114, longitude: 17.016391, }}
+        />
+      </MapView>
+
+      <RectButton style={styles.nextButton} onPress={handleNextStep}>
+        <Text style={styles.nextButtonText}>Continue</Text>
+      </RectButton>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    position: 'relative'
+  },
+
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+
+  nextButton: {
+    backgroundColor: '#15c3d6',
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 56,
+
+    position: 'absolute',
+    left: 24,
+    right: 24,
+    bottom: 40,
+  },
+
+  nextButtonText: {
+    fontFamily: 'Nunito_800ExtraBold',
+    fontSize: 16,
+    color: '#FFF',
+  }
+})
